cd8c3435efc0c2655036faea1c0990ef7fb58378
Arrthi04 <arrthim1012@gmail.com>
2024-03-30 08:28:57 +0530

    server

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-import { Navbar,TextInput,Button } from 'flowbite-react';
+import { Button, Navbar, TextInput } from 'flowbite-react';
 import{Link,useLocation} from 'react-router-dom';
 import{AiOutlineSearch} from 'react-icons/ai';
 import{FaMoon} from 'react-icons/fa';
@@ -28,22 +28,20 @@ export default function Header() {
         <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
           <FaMoon/>
         </Button>
-        <Link to="/sign-in">
-          <Button gradientDuoTone='purpletoBlue'>
-            Sign In
-          </Button>
+        <Link to='/sign-in' >
+          <Button color='green' pill>Sign In</Button>
           </Link>
-          <Navbar.Toggle/>
+          <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
            <Navbar.Link active={path==="/"} as={'div'}>
-            <Link to='/'>Home</Link>
+            <Link to="/">Home</Link>
            </Navbar.Link >
            <Navbar.Link active={path ==="/about"} as={'div'}>
-            <Link to='/about'>About</Link>
+            <Link to="/about">About</Link>
             </Navbar.Link >
             <Navbar.Link active={path ==="/projects"} as={'div'}>
-            <Link to='/projects'>Projects</Link>
+            <Link to="/projects">Projects</Link>
            </Navbar.Link >
           </Navbar.Collapse>
     </Navbar>
