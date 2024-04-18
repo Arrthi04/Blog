d8110a51761b9ea84a3bd69d8d29497b1d676e23
Arrthi04 <arrthim1012@gmail.com>
2024-04-18 18:01:42 +0530

    to handle errors

--- API/controllers/auth.controller.js
+++ API/controllers/auth.controller.js
@@ -1,11 +1,16 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 
-export const signup=async(req,res)=>{
+function errorHandler(statusCode, message) {
+    return { statusCode: statusCode, message: message };
+}
+
+export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
 
-    if(!username||!email||!password||username ==='' ||email===''|| password===''){
-        return res.status(400).json({message:'All fields are required'});
+    if(!username||!email||!password||username ==='' ||email===''|| password==='')
+    {
+        next(errorHandler(400, 'All fields are required'));
     }
 
     const hashedPassword= bcryptjs.hashSync(password,10);
@@ -20,6 +25,6 @@ export const signup=async(req,res)=>{
   await newUser.save();
  res.json('Signup successfull');
     } catch (error) {
-        res.status(500).json({message:error.message});
+        next(error);
     }
 };
