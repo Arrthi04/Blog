b15544704ef6068b78070968d4f339f4a6491cb4
Arrthi04 <arrthim1012@gmail.com>
2024-05-28 22:37:02 +0530

    add admin functionality to user

--- API/models/user.model.js
+++ API/models/user.model.js
@@ -15,7 +15,16 @@ const userSchema=new mongoose.Schema({
         type:String,
         required:true,
 },
-},{timestamps:true}
+profilePicture:{
+    type:String,
+    default:'https://th.bing.com/th?id=OIP.7O4_GREtLbxqPdJCTmfatQHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2',
+},
+isAdmin:{
+    type:Boolean,
+    default:false,
+},
+},
+{timestamps:true}
 );
 
 const User=mongoose.model('User',userSchema);
