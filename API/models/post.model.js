ab8a57130e3645eb02c0129e3ae6db605dc12e4b
Arrthi04 <arrthim1012@gmail.com>
2024-05-29 20:14:07 +0530

    add create a post api

---
+++ API/models/post.model.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        'https://th.bing.com/th/id/OIP.a5YOm_1N-oe-O025Jw4PTQHaE8?w=290&h=193&c=7&r=0&o=5&dpr=1.3&pid=1.7',
+    },
+    category: {
+        type: String,
+        default: 'uncategorized',
+      },
+      slug: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+    },
+    { timestamps: true }
+  );
+
+  
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
